945c96ec3f9c6aefb7f737c26356dfc624d96dd6
Cesar <cesar_b_03@outlook.com>
2022-09-07 22:33:46 -0300

    Delete src directory

--- src/controllers/productsControllers.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import DBProductsContainer from "../container/dbProductsContainer";
-
-export const getAll = async (req: Request, res: Response) => {
-  const products = await DBProductsContainer.getAll();
-
-  res.json(products);
-};
--- src/routes/info.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import renderInfo from '../controllers/info';
-
-const router: Router = Router();
-
-router.get('/', renderInfo);
-
-export default router
